eb5927d7f1f2ec0e00f46c0d95723def40c3cee3
DanShafman <dshafman0900@bths.edu>
2016-07-02 21:20:09 -0400

    First working version, uncommented

--- MoviesTable/ViewController.swift
+++ MoviesTable/ViewController.swift
@@ -9,45 +9,65 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     var items: [String] = ["Movie0", "Movie1", "Movie2", "Movie3"]
     var movieResponse: AnyObject?
     var movieData: NSData?
-    var movieArray: [JSON]!
+    var movieArray = [Double]()
+    var cell = UITableViewCell()
+    var json: JSON!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        Alamofire.request(.GET, "https://api.themoviedb.org/3/discover/movie", parameters: ["sort_by" : "popularity", "api_key" : "08b5d2ec7ac05e1a512f2152954d31f2"])
-        
-        .validate()
-        .responseJSON { response in
-            switch response.result {
-                case .Success:
-                    let json = JSON(response.result.value!)
-                    //print(json)
-                    let arraySize: Int = json["results"].count
-                    for num in 0...arraySize {
-                        let x = json["results"][num]["title"]
-                        print(x)
-                        self.movieArray[num] = (x)
-                    }
-                    print(self.movieArray[0])
-                
-                case .Failure(let error):
-                    print(error)
-                }
-        }
-        
+        getMovies()
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
     
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.items.count;
+        return 20;
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
-        cell.textLabel?.text = self.items[indexPath.row]
+        cell = self.tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
         return cell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
     }
+    
+    func sortByPopularity() {
+        
+    }
+    
+    func getMovies() {
+        Alamofire.request(.GET, "https://api.themoviedb.org/3/discover/movie", parameters: ["sort_by" : "popularity", "api_key" : "08b5d2ec7ac05e1a512f2152954d31f2"])
+        .validate()
+        .responseJSON { response in
+        switch response.result {
+            case .Success:
+                self.json = JSON(response.result.value!)
+                print(self.findMovieByPopularity(1.000826))
+                let arraySize: Int = self.json["results"].count
+                for num in 0...arraySize - 1 {
+                    let x = self.json["results"][num]["popularity"]
+                    self.movieArray.append(x.double!)
+                    self.movieArray.sortInPlace()
+                }
+                for num in 0...arraySize - 1 {
+                    let path = NSIndexPath(forRow: num, inSection: 0)
+                    let cell = self.tableView.cellForRowAtIndexPath(path)
+                    cell?.textLabel?.text = (self.findMovieByPopularity(self.movieArray[num]) + ":    " + String(self.movieArray[num]))
+                }
+            case .Failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func findMovieByPopularity(popularity: Double) -> String {
+        for num in 0...json["results"].count {
+            if self.json["results"][num]["popularity"].double! == popularity {
+                return self.json["results"][num]["title"].string!
+            }
+        }
+        return "null"
+    }
+    
 }
